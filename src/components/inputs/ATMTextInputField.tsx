1604c2068de8eb6e6b8a5d9a99694fd8da7e1429
Nadeem khan <khannadeem1892@gmail.com>
2024-11-08 00:37:17 +0530

    add dir inputs

---
+++ src/components/inputs/ATMTextInputField.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ATMTextInputField = (props: Props) => {
+  return (
+    <div>ATMTextInputField</div>
+  )
+}
+
+export default ATMTextInputField
